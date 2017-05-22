d70e182d1ac95d6792ca61b5a9926b8bd8073e5a
Dean Zhao <zestxjest@outlook.com>
2017-05-22 20:25:13 +0800

    Multiple files

--- src/main/java/com/awss3/sample/S3Sample.java
+++ src/main/java/com/awss3/sample/S3Sample.java
@@ -1,5 +1,6 @@
 package com.awss3.sample;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
@@ -10,6 +11,11 @@ import com.amazonaws.services.s3.model.CreateBucketRequest;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.transfer.MultipleFileUpload;
+import com.amazonaws.services.s3.transfer.Transfer;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+import com.amazonaws.services.s3.transfer.Upload;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -19,19 +25,25 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 public class S3Sample {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException, InterruptedException {
         // new AmazonS3Client() is depracated.
         AmazonS3 s3 = AmazonS3ClientBuilder
                 .standard()
                 .withRegion(Regions.AP_SOUTHEAST_1)
                 .build();
 
-        try{
+        TransferManager transferManager = TransferManagerBuilder
+                .standard()
+                .withS3Client(s3)
+                .withShutDownThreadPools(true)
+                .build();
+         try{
             // list all buckets
             for (Bucket item: s3.listBuckets()){
                 System.out.println(" = " + item.getName());
@@ -44,19 +56,26 @@ public class S3Sample {
             }
 
             // create a file on S3
-            final String key = "MyObjectkey" + UUID.randomUUID();
+            final String key = "s3putObject" + UUID.randomUUID();
             s3.putObject(new PutObjectRequest(bucketName, key, createSampleFile()));
 
             // download the file
             S3Object object = s3.getObject(new GetObjectRequest(bucketName, key));
             displayTextInputStream(object.getObjectContent());
 
-        } finally {
+            // upload a single file by TransferManager
+            uploadSingleFileViaTransferManager(transferManager, bucketName,"transferManagerSingleFile"+ UUID.randomUUID());
 
+            uploadMultipleFilesViaTransferManager(transferManager, bucketName, "transferManagerMultiFile"+ UUID.randomUUID());
+
+        } finally {
+            transferManager.shutdownNow(true);
         }
     }
 
     private static File createSampleFile() throws IOException{
+        // The most reliable way to avoid a ResetException is to provide data by using a File or FileInputStream,
+        // which the AWS SDK for Java can handle without being constrained by mark and reset limits.
         File file = File.createTempFile("aws-java-sdk-", ".txt");
         file.deleteOnExit();
 
@@ -77,5 +96,46 @@ public class S3Sample {
             System.out.println("    " + line);
         }
         System.out.println();
+    }
+
+    private static void uploadSingleFileViaTransferManager(TransferManager transferManager, String bucketName, String key)
+            throws IOException, InterruptedException {
+        File file = createSampleFile();
+
+        try {
+            Upload xfer = transferManager.upload(bucketName, key, file);
+            xfer.waitForCompletion();
+            // loop with Transfer.isDone()
+            //  or block with Transfer.waitForCompletion()
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        }
+        System.out.println("Single file uploaded by transfer manager");
+    }
+
+    private static void uploadMultipleFilesViaTransferManager(TransferManager transferManager, String bucketName, String key)
+            throws IOException, InterruptedException {
+        ArrayList<File> files = new ArrayList<File>();
+        files.add(createSampleFile());
+        files.add(createSampleFile());
+        files.add(createSampleFile());
+        for (File file: files){
+            file.deleteOnExit();
+        }
+        try {
+            MultipleFileUpload xfer = transferManager.uploadFileList(bucketName,
+                    key,
+                    new File("."),
+                    files
+            );
+            xfer.waitForCompletion();
+            // loop with Transfer.isDone()
+            //  or block with Transfer.waitForCompletion()
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        }
+        System.out.println("Multi file uploaded by transfer manager");
     }
 }
